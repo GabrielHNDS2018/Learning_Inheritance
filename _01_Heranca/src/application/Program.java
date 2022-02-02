7daf05e2dfab65273e36cf07b91fea954e97888c
GHNDS <gabriel.private1998@gmail.com>
2022-02-02 18:33:34 -0300

    Version_01_Sobreposition_Override_Super
    Utilizando Sobreposição usando Override e aprendendo sobre Super();

--- _01_Heranca/src/application/Program.java
+++ _01_Heranca/src/application/Program.java
@@ -9,41 +9,18 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		Account acc = new Account(1001, "Alex", 0.0);
-		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
+		Account acc1 = new Account(1001, "Alex", 1000.00);
 		
-		//UPCASTING
+		acc1.withdraw(200.00);
+		System.out.println(acc1.getBalance());
 		
-		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
-		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.1);
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.00, 0.01);
+		acc2.withdraw(200);
+		System.out.println(acc2.getBalance());
 		
-		//DOWNCASTING
+		Account acc3 = new BusinessAccount(1003, "Bob", 1000.00, 500.00);
+		acc3.withdraw(200);
+		System.out.println(acc3.getBalance());
 		
-		//É necessário realizar o casting manual 
-		//da classe escolhida para ser aceita pelo compilador
-		
-		BusinessAccount acc4 = (BusinessAccount)acc2;
-		acc4.loan(100.00);
-		
-		//Downcasting normal, mas, acc3 foi diagnosticada como SavingsAccount
-		//Resumindo o compilador não detecta o acc3 e só vai sar erro se for executada
-		
-		// ERRO -> BusinessAccount acc5 = (BusinessAccount)acc3;
-		
-		//Falhou, pois, acc3 não é instancia de BusinessAccount
-		if(acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.00);
-			System.out.println("Loan!");
-		}
-		
-		//FORMA CORRETA!!!
-		if(acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount)acc3;
-			acc5.updateBalance();
-			System.out.println("Update!");
-		}
 	}
-
 }
