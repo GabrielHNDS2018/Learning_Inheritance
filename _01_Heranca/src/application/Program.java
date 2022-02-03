5b27fae0260ee5b90309c8cda8b53e53e685675c
GHNDS <gabriel.private1998@gmail.com>
2022-02-03 10:59:28 -0300

    Version_01_Polymorphism
    Execução da implementação de um polimorfismo referente a duas contas com o mesmo objeto

--- _01_Heranca/src/application/Program.java
+++ _01_Heranca/src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
@@ -9,9 +8,9 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		Account acc1 = new Account(1001, "Alex", 1000.00);
+		//Account acc1 = new Account(1001, "Alex", 1000.00);
 		
-		acc1.withdraw(200.00);
+		/*acc1.withdraw(200.00);
 		System.out.println(acc1.getBalance());
 		
 		Account acc2 = new SavingsAccount(1002, "Maria", 1000.00, 0.01);
@@ -20,7 +19,15 @@ public class Program {
 		
 		Account acc3 = new BusinessAccount(1003, "Bob", 1000.00, 500.00);
 		acc3.withdraw(200);
-		System.out.println(acc3.getBalance());
+		System.out.println(acc3.getBalance());*/
+		
+		Account x = new Account(1020, "Alex", 1000.00);
+		Account y = new SavingsAccount(1023, "Maria", 1000.00, 0.01);
+		x.withdraw(50);
+		y.withdraw(50);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 		
 	}
 }
